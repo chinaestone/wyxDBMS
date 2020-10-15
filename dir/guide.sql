dac15c83d9eed921147a2a30515c8a01e12e06a7
Estone <iestone@yeah.net>
2020-10-15 16:16:15 +0800

    添加guide数据库

---
+++ dir/guide.sql
@@ -0,0 +1,47 @@
+
+
+
+
+-- 完整建表及增删改查语句
+
+create table jdb_user (id int, name varchar, age int);
+
+insert into jdb_user values(1,"小鹤",20);
+
+insert into jdb_user values(2,"大刘",28);
+
+select * from jdb_user;
+
+
+
+--
+-- insert语句中已有完整的索引插入实现
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
